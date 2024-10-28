626f8d7d407f3f219af6819b4a443e5122e7b927
CameronArch <cameronarch598@gmail.com>
2024-10-28 14:05:16 -0700

    Server delete expense

--- server/src/expenses/expense-utils.ts
+++ server/src/expenses/expense-utils.ts
@@ -19,7 +19,20 @@ export function createExpenseServer(req: Request, res: Response, expenses: Expen
 }
 
 export function deleteExpense(req: Request, res: Response, expenses: Expense[]) {
-    // TO DO: Implement deleteExpense function
+    const { id, cost, description } = req.body;
+
+    if (!description || !id || !cost) {
+        return res.status(400).send({ error: "Missing required fields" });
+    }
+
+    const newExpense: Expense = {
+        id: id,
+        description,
+        cost,
+    };
+
+    expenses.delete(newExpense);
+    res.status(201).send(newExpense);   
 }
 
 export function getExpenses(req: Request, res: Response, expenses: Expense[]) {
